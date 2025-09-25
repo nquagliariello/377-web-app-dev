9c74c66f7c44403424297f9249df7878f7877449
Nicholas Quagliariello <nquagliarillo27@hanoverstudents.org>
2025-09-25 12:46:48 -0400

    hello

---
+++ 2-equation-calculator/scripts.js
@@ -0,0 +1,24 @@
+function calculateVol(){
+    let radius = $('#radius').val();
+    let height = $('#height').val();
+    let volume = ((1/3) * (3.14159) * ((radius * radius) * (height)));
+    $('#volume').html(volume);
+        
+}
+
+function calculateSH(){
+    let radius = $('#radius').val();
+    let height = $('#height').val();
+
+    let slantheight = (Math.sqrt((radius * radius) + (height * height)));
+    $('#slantheight').html(slantheight);
+
+}
+
+function calculateSA(){
+    let radius = $('#radius').val();
+    let height = $('#height').val();
+
+    let surfacea = (((3.1159) * (radius)) * (radius + slantheight));
+    $('#surfacea').html(surfacea);
+}
